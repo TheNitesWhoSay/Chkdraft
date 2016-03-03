76f257874ee86d79c1ef2cc775073e6dcb2c190d
jjf28 <forsbergjj@gmail.com>
2016-03-02 23:07:40 -0600

    Cleaned up TerrainPalette
    Separated huge switch into several functions, used more Windows UI
    features, shortened logic for some functions.

--- Chkdraft/src/TerrainPalette.h
+++ Chkdraft/src/TerrainPalette.h
@@ -12,7 +12,19 @@ class TerrainPaletteWindow : public ClassDialog
         void SetTileDialog(HWND hWnd);
 
     protected:
-        void DoScroll(HWND hWnd);
+        void Activate(WPARAM wParam);
+        void DoScroll();
+        void DoPaint();
+        void LButtonDown(WPARAM wParam, int xc, int yc);
+        void MouseWheel(WPARAM wParam);
+        void GetMinMaxInfo(LPARAM lParam);
+        void VScroll(WPARAM wParam);
+        void VScrollLineDown();
+        void VScrollLineUp();
+        void VScrollPageDown();
+        void VScrollPageUp();
+        void VScrollThumbPos();
+        void VScrollThumbTrack();
         BOOL DlgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
     private:
