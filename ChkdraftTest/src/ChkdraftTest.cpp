98ae62d94e5c03ab760834fb62034bd769e209ef
Justin F <forsbergjj@gmail.com>
2018-02-10 18:28:43 -0500

    Remove Test Files
    The test adapter had major issues, searching for new testing options

--- ChkdraftTest/src/ChkdraftTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "../../Chkdraft/src/Chkdraft.h"
-
-TEST(ConstructChkdraft, Basic) {
-    Chkdraft chkdraft;
-    EXPECT_EQ(chkdraft.CurrDialog(), (HWND)NULL);
-    EXPECT_FALSE(chkdraft.EditFocused());
-}
