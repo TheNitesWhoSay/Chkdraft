9cd3122b207fb085c5feb93fe82efcafe79ab12f
Justin F <forsbergjj@gmail.com>
2025-06-04 11:22:50 -0500

    Remove the now redundant unit/sprite properties directories

--- src/chkdraft/ui/dialog_windows/layers/sprite_properties.h
+++ src/chkdraft/ui/dialog_windows/layers/sprite_properties.h
@@ -3,7 +3,6 @@
 #include <common_files/common_files.h>
 #include <windows/windows_ui.h>
 #include <mapping_core/mapping_core.h>
-#include "preserved_sprite_stats.h"
 #include "ui/chkd_controls/player_dropdown.h"
 
 enum class SpriteListColumn;
