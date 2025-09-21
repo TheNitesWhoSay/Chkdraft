42165ed92c27c33bddc827a627a579ec83e38a26
Justin F <forwardtojj@gmail.com>
2025-09-21 17:55:38 -0500

    Add a draft implementation for FolderArchive
    - Add a draft implementation for FolderArchive
    - Fix a comment for mpq file

--- src/mapping_core/folder_archive.cpp
+++ src/mapping_core/folder_archive.cpp
@@ -1,3 +1,199 @@
 #include "folder_archive.h"
+#include <filesystem>
+#include <fstream>
 
+FolderArchive::FolderArchive(bool deleteOnClose) : ArchiveFile(deleteOnClose)
+{
 
+}
+
+FolderArchive::~FolderArchive()
+{
+    close();
+}
+
+const std::string & FolderArchive::getFilePath() const
+{
+    return this->filePath;
+}
+
+bool FolderArchive::isOpen() const
+{
+    return !this->filePath.empty() && isDirectory(this->filePath);
+}
+
+bool FolderArchive::isOpen(const std::string & filePath) const
+{
+    return !this->filePath.empty() && this->filePath == filePath && isDirectory(this->filePath);
+}
+
+bool FolderArchive::isValid(const std::string & filePath) const
+{
+    return !filePath.empty() && isDirectory(filePath);
+}
+
+bool FolderArchive::create(const std::string & filePath)
+{
+    if ( this->filePath == filePath )
+        return true;
+    else if ( filePath.empty() )
+        return false;
+
+    if ( isSamePath(this->filePath, filePath) ) // Existing directory effectively the same as the previous directory, but with a new path string
+    {
+        if ( isDirectory(filePath) )
+        {
+            this->filePath = filePath;
+            return true;
+        }
+        else
+        {
+            if ( makeDirectory(filePath) )
+            {
+                this->filePath = filePath;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+    else if ( isDirectory(filePath) ) // Existing directory different from previous referred directory
+    {
+        if ( isOpen() ) // Close the current directory
+            close();
+
+        this->filePath = filePath;
+        return true;
+    }
+    else if ( makeDirectory(filePath) ) // Non-existing directory
+    {
+        this->filePath = filePath;
+        return true;
+    }
+    else
+        return false;
+}
+
+bool FolderArchive::open(const std::string & filePath, bool readOnly, bool createIfNotFound)
+{
+    if ( isOpen(filePath) )
+        return true;
+
+    close();
+    bool isDir = ::isDirectory(filePath);
+    if ( createIfNotFound && !isDir )
+        return create(filePath);
+    else if ( isDir )
+    {
+        this->filePath = filePath;
+        return true;
+    }
+    return false;
+}
+
+void FolderArchive::close()
+{
+    if ( isOpen() )
+        removeFile(this->filePath);
+}
+
+bool FolderArchive::findFile(const std::string & subFolderPath) const
+{
+    return isOpen() && ::findFile(makeSystemFilePath(this->filePath, subFolderPath));
+}
+
+bool FolderArchive::findFile(const std::string & filePath, const std::string & subFolderPath) const
+{
+    return ::findFile(makeSystemFilePath(this->filePath, subFolderPath));
+}
+
+size_t FolderArchive::getFileSize(const std::string & subFolderPath) const
+{
+    if ( isOpen() )
+    {
+        try {
+            return std::filesystem::file_size(makeSystemFilePath(this->filePath, subFolderPath));
+        } catch ( ... ) {}
+    }
+    return 0;
+}
+
+bool FolderArchive::getFile(const std::string & subFolderPath, ByteBuffer & fileData, bool) const
+{
+    if ( isOpen() && this->findFile(subFolderPath) )
+    {
+        size_t fileSize = getFileSize(subFolderPath);
+        fileData.expand(fileSize);
+        if ( fileSize == 0 )
+            return true;
+        else
+        {
+            std::ifstream inFile(std::filesystem::path(asUtf8(makeSystemFilePath(this->filePath, subFolderPath))), std::ios_base::binary|std::ios_base::in);
+            inFile.read((char*)&fileData[0], std::streamsize(fileSize));
+            return !inFile.fail();
+        }
+    }
+    return false;
+}
+
+std::optional<std::vector<u8>> FolderArchive::getFile(const std::string & subFolderPath, bool) const
+{
+    return isOpen() ? fileToBuffer(makeSystemFilePath(this->filePath, subFolderPath)) : std::optional<std::vector<u8>>(std::nullopt);
+}
+
+bool FolderArchive::extractFile(const std::string & subFolderPath, const std::string & systemFilePath, bool) const
+{
+    return isOpen() && makeFileCopy(makeSystemFilePath(this->filePath, subFolderPath), systemFilePath);
+}
+
+bool FolderArchive::addFile(const std::string & subFolderPath, std::stringstream & fileData)
+{
+    if ( isOpen() )
+    {
+        fileData.unsetf(std::ios_base::skipws);
+        auto start = std::istream_iterator<u8>(fileData);
+        std::vector<u8> fileBytes(start, std::istream_iterator<u8>());
+        return addFile(subFolderPath, fileData);
+    }
+    return false;
+}
+
+bool FolderArchive::addFile(const std::string & subFolderPath, const std::vector<u8> & fileData)
+{
+    return isOpen() && bufferToFile(makeSystemFilePath(this->filePath, subFolderPath), fileData);
+}
+
+bool FolderArchive::addFile(const std::string & subFolderPath, const std::string & filePath)
+{
+    return isOpen() && makeFileCopy(filePath, makeSystemFilePath(this->filePath, subFolderPath));
+}
+
+bool FolderArchive::renameFile(const std::string & subFolderPath, const std::string & newSubFolderPath)
+{
+    if ( isOpen() )
+    {
+        if ( subFolderPath == newSubFolderPath )
+            return false;
+
+        std::string oldPath = makeSystemFilePath(this->filePath, subFolderPath);
+        std::string newPath = makeSystemFilePath(this->filePath, newSubFolderPath);
+        if ( isSamePath(oldPath, newPath) ) // Different strings but same effective path
+            return false;
+        else if ( !::findFile(oldPath) )
+            return false;
+        else
+            return makeFileCopy(oldPath, newPath) && removeFile(oldPath);
+    }
+    return false;
+}
+
+bool FolderArchive::removeFile(const std::string & subFolderPath)
+{
+    if ( isOpen() )
+    {
+        std::string fullPath = makeSystemFilePath(this->filePath, subFolderPath);
+        if ( ::findFile(fullPath) )
+            return ::removeFile(fullPath);
+    }
+    return false;
+}
--- src/mapping_core/folder_archive.h
+++ src/mapping_core/folder_archive.h
@@ -1,6 +1,101 @@
 #ifndef FOLDERARCHIVE_H
 #define FOLDERARCHIVE_H
+#include "basics.h"
+#include "file_browser.h"
+#include "archive_file.h"
+#include <memory>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <vector>
 
+typedef void* HANDLE;
 
+class FolderArchive;
+using FolderArchivePtr = std::shared_ptr<FolderArchive>;
+
+class FolderArchive : public ArchiveFile
+{
+public:
+    // Constructs a folder archive, if deleteOnClose is specified the folder is deleted when close() is called
+    FolderArchive(bool deleteOnClose = false);
+
+    // Calls close()
+    virtual ~FolderArchive() override;
+
+    // Gets the path of the folder to which this FolderArchive refers, this may be empty if no folder is set
+    virtual const std::string & getFilePath() const override;
+
+    // Returns true if this FolderArchive refers to a valid folder path
+    virtual bool isOpen() const override;
+
+    // Returns true if this FolderArchive refers to a valid folder path equivalent to filePath
+    virtual bool isOpen(const std::string & filePath) const override;
+
+    // Checks whether a valid folder exists at the given path
+    virtual bool isValid(const std::string & filePath) const override;
+
+    // Creates a folder at the given filePath and makes it the folder to which this FolderArchive refers
+    virtual bool create(const std::string & filePath);
+
+    // Checks whether a folder exists at the given path, and makes it the folder path for this FolderArchive if so
+    // If the given folder path is the folder to which this FolderArchive already refers, no operation occurs and the method returns true
+    // If the given folder path is a different valid folder from the one currently referred to by this FolderArchive, the current is closed and the new set
+    virtual bool open(const std::string & filePath, bool readOnly = false, bool createIfNotFound = false) override;
+
+    // Does nothing except return true
+    virtual inline bool save() override { return true; }
+
+    // Clears the folder path to which this archive refers
+    // If no folder path has been set calling this method has no effect
+    // If the temporary flag was specified the folder is deleted
+    virtual void close() override;
+
+    // Checks whether a file exists within the FolderArchive at the given folderPath
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual bool findFile(const std::string & subFolderPath) const override;
+
+    // Checks whether a file exists within the folder given by filePath, at the given subFolderPath
+    virtual bool findFile(const std::string & filePath, const std::string & subFolderPath) const override;
+
+    // Attempts to get the file size from this FolderArchive at subFolderPath
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual size_t getFileSize(const std::string & subFolderPath) const override;
+
+    // Attempts to get a file from this FolderArchive at subFolderPath and place the data within the fileData buffer
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual bool getFile(const std::string & subFolderPath, ByteBuffer & fileData, bool = false) const override;
+
+    // Attempts to get a file from this FolderArchive at subFolderPath and return the file data
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual std::optional<std::vector<u8>> getFile(const std::string & subFolderPath, bool = false) const override;
+
+    // Attempts to copy a file from this FolderArchive at subFolderPath to a new file at systemFilePath
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual bool extractFile(const std::string & subFolderPath, const std::string & systemFilePath, bool = false) const override;
+
+    // Attempts to add a file to this folder at subFolderPath with the data from the fileData buffer
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual bool addFile(const std::string & subFolderPath, std::stringstream & fileData) override;
+
+    // Attempts to add a file to this folder at subFolderPath with the data from the fileData buffer
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual bool addFile(const std::string & subFolderPath, const std::vector<u8> & fileData) override;
+
+    // Attempts to add the file at filePath to the folder at subFolderPath
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual bool addFile(const std::string & subFolderPath, const std::string & filePath) override;
+
+    // Attempts to move the file in this folder at subFolderPath to a sub-folder in this folder matching newSubFolderPath
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual bool renameFile(const std::string & subFolderPath, const std::string & newSubFolderPath) override;
+
+    // Attempts to delete the file in this folder at subFolderPath
+    // Cannot be used unless the FolderArchive already refers to a valid folder
+    virtual bool removeFile(const std::string & subFolderPath) override;
+
+private:
+    std::string filePath {};
+};
 
 #endif
