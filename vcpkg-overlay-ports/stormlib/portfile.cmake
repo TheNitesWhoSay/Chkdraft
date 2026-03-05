5447b22df35b577636263c824d76d18915ca6382
Justin F <forwardtojj@gmail.com>
2026-03-05 02:00:01 -0600

    Reorg builds and add ASAN build
    - Move target triplets, runtime library and toolchain file to CMakePresets
    - Factor out build details so that builds like x64-debug/x64-release are made by inheriting from several different, reusable builds
    - Add the x64-debug-asan build (which requires non-static linkage to work with vcpkg)
    - Add the asan flag set in presets to individual projects compile options
    - Supply the DLL required for proper gtest test discovery to occur when using ASAN
    - Add a switch for whether the static or non-static casc lib is used
    - Update storm to a fork that solves the non-static linker issue related to addListFileEntries
    - Update to the newer no-auto-link flag name for storm

--- vcpkg-overlay-ports/stormlib/portfile.cmake
+++ vcpkg-overlay-ports/stormlib/portfile.cmake
@@ -2,9 +2,9 @@ message("using vcpkg overlay port for stormlib")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO ladislav-zezula/StormLib
-    REF 539a04e06578ce9b0cf005446eff66e18753076d
-    SHA512 4d1a1c8eb559e7b685a3e7679bef2440af5bbed6c93157b6b6f2acce47d7249ef12643f4cd4c7ec341ea40b07e5109bdc5f99b34c9a536fa5d878430973af867
+    REPO TheNitesWhoSay/StormLib
+    REF 720941a94c66a4157bd368fdca361d143e9db63d
+    SHA512 4efe16f8d09f01ce9bcc947d58bd27b85a23bd434c4db1f97502e16c52db862fabe2e677442495ba61589da01eef68be1f1f2ebf3a302d1c968f5145c7457dd4
     HEAD_REF master
 )
 
